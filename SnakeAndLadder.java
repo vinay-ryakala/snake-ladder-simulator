fa73126437630122fdaf6e9e20e17fdf8c3e01c6
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-29 15:09:45 +0530

    ADD:To get a number using random UC2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeandLadder
 		int INTIAL_POSITION = 0;
 		int NUMBER_OF_PLAYER = 1;
 		System.out.println("Welcome to Snake and Ladder Simulation");
+		int dieRoll = (int) ((Math.random() * 6 ) + 1 );
+		System.out.println("Number got by rolling a die: "+dieRoll);
 	}
 }
