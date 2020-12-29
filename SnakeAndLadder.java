b5db8b675c9fdb6422ba62bbb70d07adc0a38251
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-29 14:53:10 +0530

    ADD: Intial position and number of players UC1

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,6 +2,8 @@ public class SnakeandLadder
 {
 	public static void main(String[] args)
 	{
+		int INTIAL_POSITION = 0;
+		int NUMBER_OF_PLAYER = 1;
 		System.out.println("Welcome to Snake and Ladder Simulation");
 	}
 }
